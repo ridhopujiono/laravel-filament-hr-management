720ac0790cabd0a20c67affd66289496f82085a9
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-05-04 15:16:16 +0700

    feat: add employee data fields to users table

--- database/migrations/2025_05_04_081149_added_employee_data_into_users_table.php
+++ database/migrations/2025_05_04_081149_added_employee_data_into_users_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->date('join_date')->nullable();
+            $table->text('photo')->nullable();
+            $table->boolean('is_active')->default(true);
         });
     }
 
@@ -22,7 +26,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+            $table->dropColumn('join_date');
+            $table->dropColumn('photo');
+            $table->dropColumn('is_active');
         });
     }
 };
