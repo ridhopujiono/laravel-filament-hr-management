0e88f4064a2abe8498cc392a22ea7e2d668462c6
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-05-04 15:22:11 +0700

    feat(user): add user profile fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'join_date',
+        'photo',
+        'is_active',
     ];
 
     /**
@@ -43,6 +48,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 }
